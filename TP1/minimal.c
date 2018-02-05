365c753277c7660856056908f974f0d492997043
CUSUMANO NICOLAS <ncusuman@localhost.localdomain>
2018-02-05 12:53:12 +0100

    fin de seance

--- TP1/minimal.c
+++ TP1/minimal.c
@@ -5,14 +5,22 @@
 #include <stdio.h>
 
 /* Dimensions de la fenêtre */
-static unsigned int WINDOW_WIDTH = 800;
-static unsigned int WINDOW_HEIGHT = 600;
+static unsigned int WINDOW_WIDTH = 400;
+static unsigned int WINDOW_HEIGHT = 400;
 
 /* Nombre de bits par pixel de la fenêtre */
 static const unsigned int BIT_PER_PIXEL = 32;
 
 /* Nombre minimal de millisecondes separant le rendu de deux images */
 static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
+
+/* Fonctions */
+void onResize() {
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-1., 1., -1., 1.);
+}
 
 int main(int argc, char** argv) {
 
@@ -21,28 +29,39 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Impossible d'initialiser la SDL. Fin du programme.\n");
         return EXIT_FAILURE;
     }
-    
+
     /* Ouverture d'une fenêtre et création d'un contexte OpenGL */
-    if(NULL == SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER)) {
+    if(NULL == SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE)) {
         fprintf(stderr, "Impossible d'ouvrir la fenetre. Fin du programme.\n");
         return EXIT_FAILURE;
     }
-    
+
     /* Titre de la fenêtre */
-    SDL_WM_SetCaption("OpenGL powa :D", NULL);
-    
+    SDL_WM_SetCaption("Padawan OpenGL", NULL);
+
     /* Boucle d'affichage */
     int loop = 1;
+    float r = 0;
+    float g = 0;
+    float b = 0;
+    int x = 0;
+    int y = 0;
+    glClear(GL_COLOR_BUFFER_BIT);
     while(loop) {
 
         /* Récupération du temps au début de la boucle */
         Uint32 startTime = SDL_GetTicks();
-        
+
         /* Placer ici le code de dessin */
-        
+        glClearColor(r, g, b, 1);
+        glColor3ub(r * 255, v * 255, b * 255);
+        glBegin(GL_POINTS);
+            glVertex2f(-1 + 2. * x / WINDOW_WIDTH, -(-1 + 2. * y / WINDOW_HEIGHT));
+        glEnd();
+
         /* Echange du front et du back buffer : mise à jour de la fenêtre */
         SDL_GL_SwapBuffers();
-        
+
         /* Boucle traitant les evenements */
         SDL_Event e;
         while(SDL_PollEvent(&e)) {
@@ -52,18 +71,37 @@ int main(int argc, char** argv) {
                 loop = 0;
                 break;
             }
-            
+
             /* Quelques exemples de traitement d'evenements : */
             switch(e.type) {
 
                 /* Clic souris */
                 case SDL_MOUSEBUTTONUP:
                     printf("clic en (%d, %d)\n", e.button.x, e.button.y);
+                    x = e.button.x;
+                    y = e.button.y;
+                    break;
+
+                case SDL_MOUSEMOTION:
+                    r = e.button.x % 255 / 255.0;
+                    g = e.button.y % 255 / 255.0;
+                    b = (e.button.x + e.button.y) % 255 / 255.0;
                     break;
 
                 /* Touche clavier */
                 case SDL_KEYDOWN:
                     printf("touche pressée (code = %d)\n", e.key.keysym.sym);
+                    if(e.key.keysym.sym == 113) {
+                        loop = 0;
+                        break;
+                    }
+                    break;
+
+                case SDL_VIDEORESIZE:
+                    WINDOW_HEIGHT = e.resize.h;
+                    WINDOW_WIDTH = e.resize.w;
+                    onResize();
+                    SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE);
                     break;
 
                 default:
@@ -80,7 +118,7 @@ int main(int argc, char** argv) {
         }
     }
 
-    /* Liberation des ressources associées à la SDL */ 
+    /* Liberation des ressources associées à la SDL */
     SDL_Quit();
 
     return EXIT_SUCCESS;
